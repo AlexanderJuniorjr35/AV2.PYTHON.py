3f70a97ab2ad3649f2e0fba888bdb8c162c0dd6c
AlexanderJuniorjr35 <114843938+AlexanderJuniorjr35@users.noreply.github.com>
2022-11-10 10:41:29 -0300

    Create AV2 N2 Imposto de produto.py

---
+++ AV2 N2 Imposto de produto.py
@@ -0,0 +1,13 @@
+import time
+
+def soma_imposto(taxa_imposto, Custo):
+    return (1 + taxa_imposto/100)*Custo
+
+print("\nCalculo Imposto sobre o produto")
+time.sleep(2)
+
+tax = float(input("\nDigite a TAXA em porcentagem de imposto: "))
+cus = float(input("\nDigite o CUSTO do produto: "))
+
+print(f"\nValor do produto com imposto: {soma_imposto(tax,cus):.2f}")
+time.sleep(10)
